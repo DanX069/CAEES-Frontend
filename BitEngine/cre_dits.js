333a2dc91f76f674bb94e33c4e93e31794046a35
Danish Mehmood <danishm21@iitk.ac.in>
2023-03-21 20:01:38 +0530

    Create cre_dits.js

---
+++ BitEngine/cre_dits.js
@@ -0,0 +1,19 @@
+// Redirect to Entry/Exit page
+document.querySelector(".nav-item:nth-child(1)").addEventListener("click", function() {
+    window.location.href = "homepage.html";
+  });
+
+  // Redirect to History page
+  document.querySelector(".nav-item:nth-child(2)").addEventListener("click", function() {
+    window.location.href = "history.html";
+  });
+
+  // Redirect to Credits page
+  document.querySelector(".nav-item:nth-child(3)").addEventListener("click", function() {
+    window.location.href = "credits.html";
+  });
+
+  // Redirect to Logout page
+  document.querySelector(".nav-item:nth-child(4)").addEventListener("click", function() {
+    window.location.href = "index.html";
+  });
